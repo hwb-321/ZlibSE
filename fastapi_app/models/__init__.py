1e5d933379442c0bf361c24392101ee16bf76a36
4070s-linux <hwb321529212@gmail.com>
2026-03-21 13:01:39 +0800

    拆分前端分支

--- fastapi_app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .book import Book, UploadedBook, UserCollectedBook
-from .user import User
-
-__all__ = ["Book", "UploadedBook", "User", "UserCollectedBook"]
--- fastapi_app/repositories/user_repository.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import select
-from sqlalchemy.orm import Session
-
-from ..models import User
-
-
-def get_user_by_username(db: Session, username: str) -> User | None:
-    return db.execute(select(User).where(User.username == username)).scalar_one_or_none()
